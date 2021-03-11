bda27121be004cb3eae12cdda2f5e4de7a9f2e83
jagrit2008 <70403360+jagrit2008@users.noreply.github.com>
2021-03-11 21:58:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/loginScreens'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
